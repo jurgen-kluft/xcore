37f04fba2d1f14bfd2daff748ddc377b3334ea34
Jurgen <jurgen@virtuos.com.cn>
2016-09-26 16:26:17 +0800

    *: update package descriptor

--- package/package.go
+++ package/package.go
@@ -2,6 +2,7 @@ package xcore
 
 import (
 	"github.com/jurgen-kluft/xcode/denv"
+	"github.com/jurgen-kluft/xentry/package"
 	"github.com/jurgen-kluft/xbase/package"
 	"github.com/jurgen-kluft/xunittest/package"
 )
@@ -9,20 +10,26 @@ import (
 // GetPackage returns the package object of 'xcore'
 func GetPackage() *denv.Package {
 	// Dependencies
-	xbasepkg := xbase.GetPackage()
 	unittestpkg := xunittest.GetPackage()
+	entrypkg := xentry.GetPackage()
+	xbasepkg := xbase.GetPackage()
 
 	// The main (xcore) package
 	mainpkg := denv.NewPackage("xcore")
-	mainpkg.AddPackage(xbasepkg)
 	mainpkg.AddPackage(unittestpkg)
+	mainpkg.AddPackage(entrypkg)
+	mainpkg.AddPackage(xbasepkg)
 
 	// 'xcore' library
 	mainlib := denv.SetupDefaultCppLibProject("xcore", "github.com\\jurgen-kluft\\xcore")
 	mainlib.Dependencies = append(mainlib.Dependencies, unittestpkg.GetMainLib())
+	mainlib.Dependencies = append(mainlib.Dependencies, xbasepkg.GetMainLib())
 
 	// 'xcore' unittest project
 	maintest := denv.SetupDefaultCppTestProject("xcore_test", "github.com\\jurgen-kluft\\xcore")
+	maintest.Dependencies = append(maintest.Dependencies, unittestpkg.GetMainLib())
+	maintest.Dependencies = append(maintest.Dependencies, entrypkg.GetMainLib())
+	maintest.Dependencies = append(maintest.Dependencies, mainlib)
 
 	mainpkg.AddMainLib(mainlib)
 	mainpkg.AddUnittest(maintest)
